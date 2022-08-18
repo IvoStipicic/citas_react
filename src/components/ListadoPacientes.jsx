8c2afa4da72f8b91de5cf3ff66752d0b8fbf572d
Ivo Stipicic <ivo.stipicic@globant.com>
2022-08-18 16:30:45 -0300

    primer commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Paciente from "./Paciente";
+
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+  useEffect(() => {
+    console.log("Se agrego un nuevo paciente");
+  }, [pacientes]);
+
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className=" text-xl mt-5 text-center mb-10">
+            Administra tus{" "}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+          {pacientes.map((paciente) => {
+            return (
+              <Paciente
+                paciente={paciente}
+                key={paciente.id}
+                setPaciente={setPaciente}
+                eliminarPaciente={eliminarPaciente}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className=" text-xl mt-5 text-center mb-10">
+            Comienza agregando pacientes{" "}
+            <span className="text-indigo-600 font-bold">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
